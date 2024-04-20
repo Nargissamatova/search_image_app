daf61b64d3a96bb88ac41f63d3a2c6f6f4fd406f
Nargis <nargissamatova16@gmail.com>
2024-04-20 19:37:33 +0300

    api access

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import { useRef } from "react";
 import Form from "react-bootstrap/Form";
 import "./App.css";
 
+const API_URL = "<https://api.unsplash.com/search/photos>";
+
 function App() {
+  console.log("key", import.meta.env.VITE_API_KEY);
   const searchInput = useRef(null);
   const handleSearch = (event) => {
     event.preventDefault();
     console.log(searchInput.current.value);
+  };
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection;
   };
   return (
     <div className="container">
@@ -22,6 +28,12 @@ function App() {
             ref={searchInput}
           />
         </Form>
+      </div>
+      <div className="filters">
+        <div onClick={() => handleSelection("nature")}>Nature</div>
+        <div onClick={() => handleSelection("birds")}>Birds</div>
+        <div onClick={() => handleSelection("cats")}>Cats</div>
+        <div onClick={() => handleSelection("shoes")}>Shoes</div>
       </div>
     </div>
   );
