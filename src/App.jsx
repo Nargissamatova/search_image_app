ed78bc4a3a74467f8146570080a4cbaf736fbd6e
Nargis <nargissamatova16@gmail.com>
2024-04-20 22:35:42 +0300

    fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 // useRef() -> "use Reference "does not cause re-renders when its value changes. When you want a component to "remember" some information, but you don't want that information to trigger new renders: animations, handdling focus. useRef returns ref object with a single current property initially set to the initial value you provided.
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
 import "./App.css";
 
-const API_URL = "<https://api.unsplash.com/search/photos>";
+const API_URL = "https://api.unsplash.com/search/photos";
+const IMAGES_PER_PAGE = 20;
 
 function App() {
   console.log("key", import.meta.env.VITE_API_KEY);
   const searchInput = useRef(null);
+
+  useEffect(() => {
+    const getImages = async () => {};
+    getImages();
+  }, []);
+
+  const fetchImages = async () => {
+    try {
+      const result = await axios.get(
+        `${API_URL}?query=${
+          searchInput.current.value
+        }&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${
+          import.meta.env.VITE_API_KEY
+        }`
+      );
+      console.log("result", result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleSearch = (event) => {
     event.preventDefault();
     console.log(searchInput.current.value);
